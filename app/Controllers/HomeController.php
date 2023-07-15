c7e8f08258ee418b771dd5fa6cfe2ab3ff45d243
Nikita <funcrafter1337@gmail.com>
2023-07-15 10:23:03 +0300

    sd

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -9,7 +9,10 @@ use App\View;
 class HomeController
 {
     public function index(): View
-    {
+    {   
+        $db = new pdo('mysql:host=localhost;dbname=my_db', 'root', 'creatine');
+        var_dump($db)
+        exit;
         return View::make('index');
     }
 }
